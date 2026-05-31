b80f5f257364fbe7a5fcdd9c3253bc289103587d
Pavarna-Venkat <pavarna04_bai26@mepcoeng.ac.in>
2026-05-31 21:18:02 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -2,6 +2,7 @@ class Config:
 
     SECRET_KEY = "a1727e0070e64d63deaef2554d5092a2"
 
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:Pavarna%4026@localhost/lms"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///lms.db"
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
